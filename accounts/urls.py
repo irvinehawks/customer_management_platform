3af5cf23a1422fe2f8e41eb5db8873faae6d6d2f
irvine kwambana <irvenehawks@gmail.com>
2023-10-16 07:47:33 +0200

    project setup and database models migrations

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('customers/', views.customers),
+    path('products/', views.products),
+    path('orders/', views.orders),
+]
